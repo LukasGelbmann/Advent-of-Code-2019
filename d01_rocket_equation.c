1238a83f01e6c04806cafe4f59be6573955597ef
Lukas Gelbmann <me@lukasgelbmann.com>
2019-12-02 12:58:31 +0100

    Free allocated memory in day 1

--- d01_rocket_equation.c
+++ d01_rocket_equation.c
@@ -4,6 +4,7 @@
 #include "io.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 
 // Return the fuel required for some mass, including fuel needed to lift fuel.
@@ -16,31 +17,37 @@ static long recursive_fuel(long mass)
     return fuel;
 }
 
-// Print the answer to both parts.
-static error solve(FILE *in)
+// Print the answer to both parts, reallocating the buffer if needed.
+static error solve_with_buffer(FILE *in, char **buffer_ptr, size_t *size_ptr)
 {
     error err;
-    char *buffer = NULL;
-    size_t buffer_size = 0;
-
     long total_simple = 0;
     long total_recursive = 0;
-    while ((err = read_stripped_line(&buffer, &buffer_size, in)) != ERR_EOF) {
-        if (err) {
-            return err;
-        }
+    while (!(err = read_stripped_line(buffer_ptr, size_ptr, in))) {
         long mass;
-        err = parse_long(&mass, buffer);
+        err = parse_long(&mass, *buffer_ptr);
         if (err) {
             return err;
         }
         total_simple += mass / 3 - 2;
         total_recursive += recursive_fuel(mass);
     }
+    if (err != ERR_EOF) {
+        return err;
+    }
     printf("%ld\n", total_simple);
     printf("%ld\n", total_recursive);
-
     return SUCCESS;
+}
+
+// Print the answer to both parts of the puzzle.
+static error solve(FILE *in)
+{
+    char *buffer = NULL;
+    size_t buffer_size = 0;
+    error err = solve_with_buffer(in, &buffer, &buffer_size);
+    free(buffer);
+    return err;
 }
 
 // For each input file, print the answer to both parts of the puzzle.
